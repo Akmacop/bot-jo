c9333bac4c7fa0e166731494c2dec58bec2638b3
Akmacop <113163089+Akmacop@users.noreply.github.com>
2022-10-04 13:28:07 +0700

    Add files via upload

---
+++ link sc.js
@@ -0,0 +1,6 @@
+link script ada di dalam deskripsi group info.
+
+https://chat.whatsapp.com/Gjvs7L6neu415TvuTz1rVF
+
+note:
+jangan send bug di dalam grup info kalau gak mau sc yang berpasword...!!!
